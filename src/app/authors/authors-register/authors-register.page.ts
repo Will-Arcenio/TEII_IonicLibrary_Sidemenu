c785d5bc92fa24b80539607a72ddb6318e9dc4bd
Willian Arcênio <willianarcenio@hotmail.com>
2022-04-21 02:17:12 -0300

    Criando cadastro e listagem de Autores.

--- src/app/authors/authors-register/authors-register.page.ts
+++ src/app/authors/authors-register/authors-register.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthorsApiService } from '../authors-api.service';
+import { MessageService } from '../../services/message.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-authors-register',
@@ -6,10 +11,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authors-register.page.scss'],
 })
 export class AuthorsRegisterPage implements OnInit {
+  form: FormGroup;
+  loading = false;
+  edit = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private authorsApiService: AuthorsApiService,
+    private router: Router,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      id: [''],
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      nacionalidade: ['Brasileiro(a)', Validators.required],
+      redes: ['', Validators.required],
+      nascimento: [''],
+      retrato: ['', Validators.required],
+    });
+
+    // "+" para converter para número
+    const id =+ this.activatedRoute.snapshot.params.id;
+
+    this.edit = false;
+    if (id) {
+      this.edit = true;
+      this.findAuthorById(id);
+    }
+  }
+
+  findAuthorById(id: number) {
+    this.loading = true;
+    this.authorsApiService.findAuthorById(id).pipe(finalize(() => this.loading = false)).subscribe(
+      (author) => {
+        if (author) {
+          this.form.patchValue({
+            ...author,
+          });
+        }
+      },
+      () => {
+        this.messageService.showErrorMessage(`Erro ao buscar autor(a) '${id}'`, () => this.findAuthorById(id));
+      }
+    );
+  }
+
+  saveAuthor() {
+    const {nome} = this.form.value;
+    const {sobrenome} = this.form.value;
+    this.loading = true;
+
+    this.authorsApiService.saveAuthor(this.form.value).pipe(finalize(() => this.loading = false)).subscribe(
+      () => {
+
+        // Se tudo ocorrer conforme esperado, verifica se o Autor é editado ou Adicionado
+        if(this.edit) {
+          this.messageService.showSuccessMessage(`Autor(a) '${nome} ${sobrenome}' editado(a) com sucesso!`);
+        } else {
+          this.messageService.showSuccessMessage(`Autor(a) '${nome} ${sobrenome}' adicionado(a) com sucesso!`);
+        }
+        this.router.navigate(['authors-list']);
+      },
+      () => {
+        this.messageService.showErrorMessage(`Erro ao salvar o Autor(a) '${nome} ${sobrenome}'.`, () => this.saveAuthor());
+      }
+    );
   }
 
 }
