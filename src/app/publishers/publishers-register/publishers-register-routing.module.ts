173cbf43b4ea1076b5f7ca316caabafd420e24b9
Willian Arcênio <willianarcenio@hotmail.com>
2022-04-27 00:01:23 -0300

    Criação de Editoras - Ajuste nas Rotas para edição + Criação das funções (page.ts) + Ajuste na página de cadastro (HTML).

--- src/app/publishers/publishers-register/publishers-register-routing.module.ts
+++ src/app/publishers/publishers-register/publishers-register-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: PublishersRegisterPage
+  },
+  {
+    path: ':id',
+    component: PublishersRegisterPage
   }
 ];
 
--- src/app/publishers/publishers-register/publishers-register.page.ts
+++ src/app/publishers/publishers-register/publishers-register.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MessageService } from '../../services/message.service';
+import { PublishersApiService } from '../publishers-api.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-publishers-register',
@@ -6,10 +11,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publishers-register.page.scss'],
 })
 export class PublishersRegisterPage implements OnInit {
+  form: FormGroup;
+  loading = false;
+  edit; false;
 
-  constructor() { }
+  constructor(
+    private formBiulder: FormBuilder,
+    private router: Router,
+    private messageService: MessageService,
+    private activatedRoute: ActivatedRoute,
+    private publishersApiService: PublishersApiService
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBiulder.group({
+      id: [''],
+      nome: ['', Validators.required],
+      endereco: ['', Validators.required],
+      telefone: [''],
+      foto: ['', Validators.required],
+      site: [''],
+      email: ['', Validators.required],
+      localizacao: [''],
+      iframeMap: ['']
+    });
+
+    // "+" para converter para número
+    const id =+ this.activatedRoute.snapshot.params.id;
+
+    this.edit = false;
+    if (id) {
+      this.edit = true;
+      this.findPublisherById(id);
+    }
+  }
+
+  findPublisherById(id: number) {
+    this.loading = true;
+    this.publishersApiService.findPublisherById(id).pipe(finalize(() => this.loading = false)).subscribe(
+      (publisher) => {
+        if (publisher) {
+          this.form.patchValue({
+            ...publisher,
+          });
+        }
+      },
+      () => {
+        this.messageService.showErrorMessage(`Erro ao buscar editor(a) '${id}'`, () => this.findPublisherById(id));
+      }
+    );
+  }
+
+  savePublisher() {
+    const {nome} = this.form.value;
+    this.loading = true;
+
+    this.publishersApiService.savePublisher(this.form.value).pipe(finalize(() => this.loading = false)).subscribe(
+      () => {
+
+        // Se tudo ocorrer conforme esperado, verifica se a Editora é editada ou Adicionada
+        if(this.edit) {
+          this.messageService.showSuccessMessage(`Editora '${nome}' editada com sucesso!`);
+        } else {
+          this.messageService.showSuccessMessage(`Editora '${nome}' adicionada com sucesso!`);
+        }
+        this.router.navigate(['publishers-list']);
+      },
+      () => {
+        this.messageService.showErrorMessage(`Erro ao salvar a Editora '${nome}'.`, () => this.savePublisher());
+      }
+    );
   }
 
 }
